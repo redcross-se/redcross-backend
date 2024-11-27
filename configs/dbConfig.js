b1f9a9f730c468014a42f7c2a47418a3f3e6f848
henry <henryhardanz@gmail.com>
2024-11-27 22:03:47 +0200

    fix: use environment variables for sql connection

--- configs/dbConfig.js
+++ configs/dbConfig.js
@@ -1,9 +1,9 @@
 module.exports = {
   development: {
-    username: "root",
-    password: "root",
-    database: "red_cross",
-    host: "127.0.0.1",
+    username: process.env.SQL_USER,
+    password: process.env.SQL_PASS,
+    database: process.env.SQL_DATABASE,
+    host: process.env.SQL_HOST,
     port: 3306,
     dialect: "mysql",
   },
