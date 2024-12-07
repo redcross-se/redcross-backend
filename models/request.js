6fdab7b2c1e82eb2674b62a35c1a054db6f8206b
henry <henryhardanz@gmail.com>
2024-12-07 16:11:18 +0200

    hotfix: fix import statements

--- models/request.js
+++ models/request.js
@@ -1,5 +1,5 @@
-import { DataTypes } from "sequelize";
-import sequelize from "../config/database";
+const { DataTypes } = require("sequelize");
+const sequelize = require("./index");
 
 const Request = sequelize.define("Request", {
   id: {
